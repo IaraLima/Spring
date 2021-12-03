bddf425687c1279e97d08cd6a0227bddc397116f
IaraLima <iarasantos.cabluelo@gmail.com>
2021-12-03 00:10:29 -0300

    blog pessoal crud

---
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,62 @@
+package br.org.generation.blogpessoal.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity //create table no my sql
+@Table (name="tb_postagens") //colocando o nome da tabela mysql
+
+
+
+
+public class Postagem {
+
+	
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto_increment
+	private Long id;
+	
+	@NotBlank(message = "O atributo título é obrigatório!")
+	@Size(min = 5, max =100, message = "O atributo texto deve ter no ,ínimo 5 e no máximo 100 caracteres!")
+	private String texto;
+	
+	@UpdateTimestamp // a data atualiza sozinha com o sistema
+	private LocalDate data;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+	
+	
+}
